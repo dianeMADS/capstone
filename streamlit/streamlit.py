97b7bf4a2fab624a942df5820699cf97d6cfef89
jim-umich <82122021+jim-umich@users.noreply.github.com>
2021-12-19 19:19:24 -0800

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -263,7 +263,7 @@ def main():
   st.markdown(
       """For the agglomerative clustering, it appears that there are differences in the cluster groups that are clear in the average time series for each cluster especially between 2019 and 2020. The blue (0) cluster group has far greater variance than the orange (4) cluster.  We can also see that the blue cluster does not continue into 2021. In the map, we can see that most of the nodes belong to a single cluster.   The smallest clusters appear to be the least consistent sensors and have many data gaps or were only active for a short period of time. It appears that the clusters may have only separated based on measurement consistency.  Further analysis is required to understand why these nodes are producing noisier data.  It is unclear if this is due to a faulty sensor or if there are local sources of pollution that cause the spikes.  Future projects could compare these clusters to traffic patterns and industrial centers that could be producers.  It would also be beneficial to include more variables such as wind speed and direction that have an impact on the direction of pollution plume movement.  """
   )
-  st.image(Image.open('streamlit/data/AgglomorativeClusters.jpg'), caption='Agglomorative Clusters')
+  st.image(Image.open('streamlit/data/AgglomerativeClusters.jpg'), caption='Agglomorative Clusters')
 
 
 
